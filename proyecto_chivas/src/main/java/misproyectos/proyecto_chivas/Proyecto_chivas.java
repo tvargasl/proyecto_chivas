8ebc7fa381c2a8076c15241895bd82b0d08499ab
tvargasl <126362144+tvargasl@users.noreply.github.com>
2023-11-25 15:05:51 -0500

    Add files via upload

---
+++ proyecto_chivas/src/main/java/misproyectos/proyecto_chivas/Proyecto_chivas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package misproyectos.proyecto_chivas;
+
+/**
+ *
+ * @author Tomas
+ */
+public class Proyecto_chivas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
