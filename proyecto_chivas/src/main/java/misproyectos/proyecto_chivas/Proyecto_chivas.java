4c232f2a18ab90de09e4d42e37261d4e3c9ea584
tvargasl <126362144+tvargasl@users.noreply.github.com>
2023-11-26 14:12:45 -0500

    Add files via upload

--- proyecto_chivas/src/main/java/misproyectos/proyecto_chivas/Proyecto_chivas.java
+++ proyecto_chivas/src/main/java/misproyectos/proyecto_chivas/Proyecto_chivas.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package misproyectos.proyecto_chivas;
 
-/**
- *
- * @author Tomas
- */
 public class Proyecto_chivas {
 
     public static void main(String[] args) {
